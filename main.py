b5b5f633d72b455272e224a897698350532cf105
brahmihub <151893249+brahmihub@users.noreply.github.com>
2025-06-01 10:00:05 +0100

    g

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ async def tac_command(request: Request):
             "symbols": symbols
         }
         return PlainTextResponse(
-            f"GAME STARTED!-------------{format_board(board)} @{user} vs @{target} — @{player1}, you're ❌ — go first!"
+            f"GAME STARTED!-----------{format_board(board)} @{user} vs @{target} — @{player1}, you're ❌ — go first!"
         )
     else:
         pending_challenges.add(pair)
