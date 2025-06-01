09e4e4b4e8cb064fc196d3e8e34c25c319c67bd0
brahmihub <151893249+brahmihub@users.noreply.github.com>
2025-06-01 07:17:24 +0100

    ae

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_empty_board():
 def format_board(board):
     # Format each cell with spaces (e.g. " 1 ", " ❌ ", etc.)
     formatted = [
-        f" {cell} " if cell in ["❌", "⭕"] else f" {cell} " for cell in board
+        f"{cell}" if cell in ["❌", "⭕"] else f"  {cell} " for cell in board
     ]
 
     # Create rows
@@ -22,7 +22,7 @@ def format_board(board):
         rows.append(row)
 
     # Join rows with a visible separator (you can change this)
-    return "\n".join(rows) + "\nChoose a number!"
+    return "-"*10.join(rows) + "\nChoose a number!"
 
 
 @app.get("/tac")
